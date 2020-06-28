efbacfb8af7ea9b0516d61c21ab66d803321ce00
Panagiotis Chalatsakos <phalatsakos@gmail.com>
2020-06-28 11:45:54 +0300

    GDADV01 - Hotspot Engine MVP

--- assets/GameScripts/wph01_script.gd
+++ assets/GameScripts/wph01_script.gd
@@ -2,23 +2,16 @@ extends Area2D
 
 var count = 0
 
-#func _input(event: InputEvent) -> void:
-#	if event is InputEventMouseButton:
-#		if event.pressed:
-#			count += 1
-#			print("Click:" + str(count))
-	
 func _ready():
-	print("=======> Added script?")
-	print("Connecting event")
+	print("WPH01")
 	connect("input_event", self, "_on_ObjectTemplate_input_event")
 
 func _on_ObjectTemplate_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
 			if event.pressed:
-				print("Left button was clicked at ", event.position)
+				print("[WPH01] Left button was clicked at ", event.position)
 			else:
-				print("Left button was released")
+				print("[WPH01] Left button was released")
 		if event.button_index == BUTTON_WHEEL_DOWN:
-			print("Wheel down")
+			print("[WPH01] Wheel down")
