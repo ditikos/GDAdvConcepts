c1817f195eeebcb739c284672441579653de5a75
Panagiotis Chalatsakos <phalatsakos@gmail.com>
2020-06-29 21:06:31 +0300

    GDADV-001 - Hotspot Engine

--- assets/GameScripts/LobbyScript.gd
+++ assets/GameScripts/LobbyScript.gd
@@ -9,8 +9,16 @@ var count = 0
 #			print("Click:" + str(count))
 	
 func _ready():
-	connect("input_event", self, "_on_area_clicked")
-	
-func _on_area_clicked(event):
-	if event is InputEventMouseButton and event.pressed:
-		print("clicked on area")
+	print("=======> Added script?")
+	print("Connecting event")
+	connect("input_event", self, "_on_ObjectTemplate_input_event")
+
+func _on_ObjectTemplate_input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton:
+		if event.button_index == BUTTON_LEFT:
+			if event.pressed:
+				print("Left button was clicked at ", event.position)
+			else:
+				print("Left button was released")
+		if event.button_index == BUTTON_WHEEL_DOWN:
+			print("Wheel down")
