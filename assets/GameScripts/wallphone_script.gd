7a7cbf556b811d411eecdd9fa0f1bad36fdb1027
Panagiotis Chalatsakos <chibioni@gmail.com>
2020-07-01 22:22:52 +0300

    GDADV-002 Added second hotspot

--- assets/GameScripts/wallphone_script.gd
+++ assets/GameScripts/wallphone_script.gd
@@ -19,14 +19,14 @@ func _on_ObjectTemplate_input_event(viewport, event, shape_idx):
 		if event.button_index == BUTTON_LEFT:
 			if event.pressed:
 				print("[WPH01] Left button was clicked at ", event.position)
-				egoSay("This is Dinos, a famous explorer.")
+				egoSay("A wall phone. It has \n203-555-5227 \nwritten in it.")
 			else:
 				print("[WPH01] Left button was released")
 		if event.button_index == BUTTON_WHEEL_DOWN:
 			print("[WPH01] Wheel down")
 
 func _on_ObjectTemplate_mouse_entered():
-	CommandText.text = "LOOK AT PAINTING"
+	CommandText.text = "LOOK AT WALL PHONE"
 	
 func _on_ObjectTemplate_mouse_exited():
 	CommandText.text = ""
